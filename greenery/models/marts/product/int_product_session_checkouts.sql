28263264672471ef4be73b8dc18c6e2e358e05ec
smikawa-ucsc <smikawa@ucsc.edu>
2023-01-30 19:19:56 +0000

    Fixed ambiguous reference in product_session_checkouts model

--- greenery/models/marts/product/int_product_session_checkouts.sql
+++ greenery/models/marts/product/int_product_session_checkouts.sql
@@ -1,7 +1,7 @@
 select 
-    product_guid, 
+    oit.product_guid, 
     count(distinct session_guid) as session_checkouts
-from {{ref('stg_order_items')}} 
+from {{ref('stg_order_items')}} oit
     join {{ref('stg_events')}} evt on oit.order_guid = evt.order_guid
 where event_description = 'checkout'
 group by 1
